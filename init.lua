e23eeb6d9e07833b002f4e52fc5ad536278f75c8
Dennis Haegler <dennis.haegler@gmail.com>
2023-12-12 13:20:37 +0100

    Change package manager to lazy vim

--- init.lua
+++ init.lua
@@ -4,9 +4,21 @@
 vim.g.mapleader = " "
 
 -- Initialisiere dein Plugin-Manager
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require('my_plugins')
+require('lazy').setup('plugins')
+
 require('mappings')
 require('config')
 
